47a7ba11fdd1407d50254e6912d76f33f3fb6aa3
Carlos Abraham <abraham@abranhe.com>
2018-08-10 11:51:40 -0400

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# [Feedback](https://feedback.abranhe.com)
+
+> i wanna hear yo'all opinions
+
+![screenshot](https://cdn.abraham.gq/projects/feedback/screenshot.png)
+
+# Team
+
+|[![Carlos Abraham Logo](https://avatars3.githubusercontent.com/u/21347264?s=50&v=4)](https://abranhe.com)|
+| :-: |
+| [Carlos Abraham](https://github.com/abranhe) |
+
+# License
+
+[MIT](https://github.com/abranhe/feedback/blob/master/LICENSE) License © [Carlos Abraham](https://github.com/abranhe/)
---
+++ index.js
@@ -0,0 +1,73 @@
+/* eslint-env browser, jquery */
+(() => {
+	if (window.location.search === '?success') {
+		$('#main').html(`
+			<h1 class="title is-2">Thanks for the feedback!</h1>
+			<p>I will get back to you as soon as possible.</p>`);
+
+		setTimeout(() => {
+			window.location.href = 'https://abranhe.com';
+		}, 3000);
+	}
+
+	// Form validation
+	$('button[type="submit"]').on('click', event => {
+		$(event.target)
+			.closest('form')
+			.find('[required]')
+			.addClass('required');
+	});
+
+	const params = new URL(location.href).searchParams;
+
+	if (params.has('product')) {
+		const title = `Feedback for ${params.get('product')}`;
+		$('#main h1').text(title);
+		$('title').text(title);
+	}
+
+	const form = $('#feedback-form');
+
+	for (const [key, value] of params) {
+		if (key === 'nameField') {
+			form.find('[name="name"]').val(value);
+			continue;
+		}
+
+		if (key === 'emailField') {
+			form.find('[name="email"]').val(value);
+			continue;
+		}
+
+		if (key === 'messageField') {
+			form.find('[name="message"]').val(value).get(0).setSelectionRange(0, 0);
+			continue;
+		}
+
+		if (key === 'extraInfo') {
+			form.append(
+				$(`<textarea style="display:none" readonly name="${key}"></textarea>`).text(value)
+			);
+			continue;
+		}
+
+		form.append(
+			$(`<input type="hidden" name="${key}">`).val(value)
+		);
+	}
+
+	form.on('submit', () => {
+		const product = params.has('product') ? (': ' + params.get('product')) : '';
+		const message = form.find('[name="message"]').val().slice(0, 100);
+		const subject = 'Feedback' + product + ' - ' + message;
+		form.prepend(
+			$(`<input type="hidden" name="_subject">`).val(subject)
+		);
+	});
+})();
+
+(() => {
+	$(document).on('click', '.navbar-burger', () => {
+		$('.navbar-burger, .navbar-menu').toggleClass('is-active');
+	});
+})();
